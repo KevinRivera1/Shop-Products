c12a344832defebc25dd12e462bd6fdd61566200
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-15 17:53:37 -0500

    feat: :art: Agregar módulos y configuración
    Se han agregado los módulos BrowserModule, AppRoutingModule, HttpClientModule y ReactiveFormsModule al AppModule para habilitar funcionalidades clave en la aplicación. Además, se ha declarado el componente AppComponent como parte del módulo.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent, AppRoutingModule } from "./index";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
