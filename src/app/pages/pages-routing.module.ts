ebfd2d2a736f96f8180cde6f8fcd94a17fa93126
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-15 17:59:11 -0500

    feat: :art: Configurar rutas para la sección de páginas
    Se han configurado las rutas para la sección de páginas en el módulo PagesRoutingModule. Esto incluye la redirección a "pages" como página principal y la definición de rutas hijas para "dashboard" y "product" con carga diferida de sus respectivos módulos.

---
+++ src/app/pages/pages-routing.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PagesComponent } from "./pages.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { ProductComponent } from "./product";
+
+const routes: Routes = [
+  { path: "", redirectTo: "pages", pathMatch: "full" },
+  {
+    path: "pages",
+    component: PagesComponent,
+    children: [
+      {
+        path: "",
+        component: ProductComponent,
+      },
+      {
+        path: "dashboard",
+        loadChildren: () =>
+          import("./dashboard/dashboard.module").then((m) => m.DashboardModule),
+      },
+      {
+        path: "product",
+        loadChildren: () =>
+          import("./product/product.module").then((m) => m.ProductModule),
+      },
+    ],
+  },
+  { path: "**", redirectTo: "pages", pathMatch: "full" },
+];
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class PagesRoutingModule {}
