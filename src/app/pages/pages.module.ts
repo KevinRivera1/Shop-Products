d5c69855210570afc1f2d1c9bb8fd4f7d8318892
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-15 18:01:16 -0500

    feat: :art: Configurar el módulo PagesModule
    En el módulo PagesModule, se han declarado y exportado los componentes CatalogueComponent, HomeComponent, ProductComponent y PagesComponent. Además, se han importado módulos y se ha configurado la estructura del módulo para incluir CommonModule, RouterModule, ProductModule, SharedModule y ReactiveFormsModule.

---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,28 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { RouterModule } from "@angular/router";
+import { SharedModule } from "../shared/shared.module";
+import { CatalogueComponent } from "./catalogue/catalogue.component";
+import { HomeComponent } from "./home/home.component";
+import { PagesComponent } from "./pages.component";
+import { ProductComponent } from "./product";
+import { ProductModule } from "./product/product.module";
+import { ReactiveFormsModule } from "@angular/forms";
+
+
+@NgModule({
+  declarations: [
+    CatalogueComponent,
+    HomeComponent,
+    ProductComponent,
+    PagesComponent
+  ],
+  exports: [
+    CatalogueComponent,
+    HomeComponent,
+    ProductComponent,
+    PagesComponent
+  ],
+  imports: [CommonModule, RouterModule, ProductModule, SharedModule, ReactiveFormsModule],
+})
+export class PagesModule {}
