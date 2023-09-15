758f2ceee640b14bde823f39761c8dfc052a9074
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-15 17:50:36 -0500

    feat: :sparkles: Agregar rutas principales y configuración de enrutamiento
    Se han definido las rutas principales en AppRoutingModule para redirigir correctamente a la página "pages" al cargar la aplicación y gestionar redirecciones no definidas. Además, se ha importado PagesRoutingModule para una configuración de enrutamiento más modular y organizada.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { PagesComponent } from "./pages/pages.component";
+import { PagesRoutingModule } from "./pages/pages-routing.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", redirectTo: "pages", pathMatch: "full" },
+  { path: "**", redirectTo: "pages", pathMatch: "full" },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), PagesRoutingModule],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
