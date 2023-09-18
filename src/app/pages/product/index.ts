c4502844d380cf4784e93118546bbae52561c156
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-18 16:56:33 -0500

    feat: :art: update Barrel de directorio product
    Se actualizo las exportaciones de directorio product

--- src/app/pages/product/index.ts
+++ src/app/pages/product/index.ts
@@ -1,3 +1,5 @@
 export * from './formProduct/formProduct.component';
+export * from './index';
 export * from './listProduct/listProduct.component';
 export * from './product.component';
+export * from './product.module';
