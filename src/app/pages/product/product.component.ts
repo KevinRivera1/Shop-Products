951f10c79c3d436867d3a0d169a923e6911020cb
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-18 17:00:44 -0500

    refactor: :art: Mejorar modularidad y comunicación con @Input y @Output
    se ha realizado una reestructuración en varios componentes con el objetivo de mejorar la modularidad y la comunicación entre ellos. Se han aplicado los siguientes cambios:  - Se han identificado las dependencias entre componentes y se han reducido las acoplamientos innecesarios. - Se ha implementado la comunicación entre componentes utilizando las directivas @Input y @Output de Angular para pasar datos de manera más efectiva. - Se han dividido componentes grandes en componentes más pequeños y reutilizables para facilitar el mantenimiento. - Se ha mejorado la claridad y la organización del código.

--- src/app/pages/product/product.component.ts
+++ src/app/pages/product/product.component.ts
@@ -23,8 +23,8 @@ export class ProductComponent implements OnInit {
   constructor(private formBuilder: FormBuilder, private productService: ProductsService) { }
 
   ngOnInit() {
-    this.IniciarForm();
-    this.obtenerProductos();
+    // this.IniciarForm();
+    // this.obtenerProductos();
   }
 
   get f() {
@@ -32,7 +32,7 @@ export class ProductComponent implements OnInit {
   }
 
 
-  IniciarForm() {
+ /*  IniciarForm() {
     this.productForm = this.formBuilder.group({
       id: [null],
       title: ['', Validators.required],
@@ -41,22 +41,21 @@ export class ProductComponent implements OnInit {
       price: ['', [Validators.required, Validators.min(0)]],
       description: ['', [Validators.required]]
     });
-  }
+  } */
 
 
-  async obtenerProductos() {
-    try {
-      const res = await firstValueFrom(this.productService.getAllProducts());
-      this.products = res;
-    } catch (err) {
-      console.log(err);
-    }
-  }
+  // async obtenerProductos() {
+  //   try {
+  //     const res = await firstValueFrom(this.productService.getAllProducts());
+  //     this.products = res;
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // }
 
 
-  saveProduct() {
+ /*  saveProduct() {
     if (this.productForm.invalid) {
-      // Manejo de errores de validación
       console.error("El formulario es inválido.");
       return;
     }
@@ -69,16 +68,16 @@ export class ProductComponent implements OnInit {
       formData.images = [];
     }
 
-    // Obtén la cadena de texto de imágenes desde el formulario
+    
     const imagesString: string = this.productForm.controls['images'].value;
 
-    // Divide la cadena por comas y elimina los espacios en blanco alrededor de cada URL
+    
     const imagesArray: string[] = imagesString.split(',').map(url => url.trim());
 
-    // Ahora, imagesArray es un array de URLs válidas que puedes enviar al servidor
+    
     console.log("Imágenes válidas:", imagesArray);
 
-    // Asegúrate de que el campo "images" en formData sea el array
+    
     formData.images = imagesArray;
 
     console.log("Formulario de datos:", formData);
@@ -86,9 +85,7 @@ export class ProductComponent implements OnInit {
     console.log("Imágenes válidas:", formData.images);
 
     if (this.selectedProduct) {
-      // Si existe un producto seleccionado, significa que estamos editando
       if (this.selectedProduct.id !== undefined) {
-        // Verifica si el registro ya existe en la base de datos por su ID
         console.log("ID del producto seleccionado:", this.selectedProduct.id);
 
         this.productService.getProductById(this.selectedProduct.id).subscribe(
@@ -96,42 +93,37 @@ export class ProductComponent implements OnInit {
             console.log("Producto existente:", existingProduct);
 
             if (existingProduct && this.selectedProduct.id !== undefined) {
-              // El registro ya existe, realiza la actualización
               console.log("Actualizando producto existente...");
 
               this.productService.updateProduct(this.selectedProduct.id, formData).subscribe({
                 next: (data) => {
                   this.response = data;
                   console.log('Producto actualizado correctamente:', this.response);
-                  
-                  this.IniciarForm();
                   this.resetProductForm();
+                  this.IniciarForm();
                 },
                 error: (error) => {
                   console.error("Error al actualizar el producto:", error);
-                  // Maneja el error y proporciona retroalimentación adecuada al usuario
                 },
                 complete: () => {
                   this.IniciarForm();
                   this.resetProductForm();
-                  return;
-                  // Realiza acciones de limpieza o redirección si es necesario
                 }
               });
             } else {
-              // El registro no existe, muestra un mensaje al usuario o maneja la situación
+              
               console.error("El producto seleccionado no existe en la base de datos.");
-              // Maneja el error y proporciona retroalimentación adecuada al usuario
+              
             }
           },
           (error) => {
             console.error("Error al verificar la existencia del producto:", error);
-            // Maneja el error y proporciona retroalimentación adecuada al usuario
+            
           }
         );
       } else {
         console.error("El producto seleccionado no tiene un ID válido.");
-        // Maneja el error y proporciona retroalimentación adecuada al usuario
+        
       }
     } else {
       console.log("Creando nuevo producto...");
@@ -146,7 +138,7 @@ export class ProductComponent implements OnInit {
         },
         error: (error) => {
           console.error("Error al crear el producto:", error);
-          // Maneja el error y proporciona retroalimentación adecuada al usuario
+          
         },
         complete: () => {
           this.productForm.reset();
@@ -156,59 +148,59 @@ export class ProductComponent implements OnInit {
         }
       });
     }
-  }
+  } */
 
 
 
-  resetProductForm() {
-    const formValues = {
-      id: null,
-      title: '',
-      price: null, // O el valor inicial adecuado para el campo price
-      description: '',
-      //images: ['https://i.ibb.co/SyQ3Szg/zeynep-sumer-lk3-F07-BN8-T8-unsplash.jpg'],
-      images: [[]],
-      category: ''
-    };
+  // resetProductForm() {
+  //   const formValues = {
+  //     id: null,
+  //     title: '',
+  //     price: null, // O el valor inicial adecuado para el campo price
+  //     description: '',
+  //     //images: ['https://i.ibb.co/SyQ3Szg/zeynep-sumer-lk3-F07-BN8-T8-unsplash.jpg'],
+  //     images: [[]],
+  //     category: ''
+  //   };
 
-    this.productForm.reset(formValues);
-  }
+  //   this.productForm.reset(formValues);
+  // }
 
-  editProduct(product: UpdateProductDto) {
-    // Asigna el producto seleccionado a la propiedad selectedProduct
-    this.selectedProduct = product;
+  // editProduct(product: UpdateProductDto) {
+  //   // Asigna el producto seleccionado a la propiedad selectedProduct
+  //   this.selectedProduct = product;
 
-    // Crea un objeto que contiene los valores del formulario
-    const formValues = {
-      id: product.id,
-      title: product.title,
-      price: product.price,
-      description: product.description,
-      images: Array.isArray(product.images) ? product.images.join(', ') : '',
-      category: product.category
-    };
+  //   // Crea un objeto que contiene los valores del formulario
+  //   const formValues = {
+  //     id: product.id,
+  //     title: product.title,
+  //     price: product.price,
+  //     description: product.description,
+  //     images: Array.isArray(product.images) ? product.images.join(', ') : '',
+  //     category: product.category
+  //   };
 
-    // Establece los valores en el formulario
-    this.productForm.setValue(formValues);
-  }
+  //   // Establece los valores en el formulario
+  //   this.productForm.setValue(formValues);
+  // }
 
 
-  deleteProduct(id: number) {
-    this.productService.deleteProduct(id).subscribe(
-      (res: any) => {
-        console.log(res);
-        this.obtenerProductos();
-      },
-      (err: any) => {
-        console.log(err);
-      }
-    );
-    console.log("delete product");
-  }
+  // deleteProduct(id: number) {
+  //   this.productService.deleteProduct(id).subscribe(
+  //     (res: any) => {
+  //       console.log(res);
+  //       this.obtenerProductos();
+  //     },
+  //     (err: any) => {
+  //       console.log(err);
+  //     }
+  //   );
+  //   console.log("delete product");
+  // }
 
-  setearForm() {
-    this.productForm.reset();
-    this.IniciarForm();
-  }
+  // setearForm() {
+  //   this.productForm.reset();
+  //   this.IniciarForm();
+  // }
 
 }
