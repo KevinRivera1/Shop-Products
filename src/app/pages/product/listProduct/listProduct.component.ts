e5da1f7c9726a8cfd44094fdbf8682a1161b041e
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-18 17:10:08 -0500

    feat: :art: Implementar funcionalidad al componente 'ListProduct'
    Se ha implementado el componente 'ListProduct' que se encarga de mostrar una lista de productos. Los cambios incluyen:  - La definición de las propiedades de entrada (@Input) para recibir la lista de productos y de salida (@Output) para emitir eventos de eliminación de productos. - La obtención de la lista de productos desde el servicio 'ProductsService' y su visualización en la plantilla. - La implementación de métodos para editar y eliminar productos, incluyendo la emisión de eventos de eliminación.  Este componente proporciona una funcionalidad esencial para la gestión de productos en la aplicación.

--- src/app/pages/product/listProduct/listProduct.component.ts
+++ src/app/pages/product/listProduct/listProduct.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ProductModel, ProductsService, UpdateProductDto } from '../../../index';
+import { firstValueFrom } from 'rxjs';
 
 @Component({
   selector: 'app-listProduct',
@@ -7,9 +9,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProductComponent implements OnInit {
 
-  constructor() { }
+  @Input() productos: ProductModel[] = [];
+  @Output() eliminarProductos = new EventEmitter();
+
+  selectedProduct: UpdateProductDto [] = [];
+  productsEdit!: UpdateProductDto;
+  loading: boolean = true;
+
+  constructor(private productService: ProductsService) { }
 
   ngOnInit() {
+    this.obtenerProductos();
+  }
+
+  async obtenerProductos() {
+    try {
+      const res = await firstValueFrom(this.productService.getAllProducts());
+      this.productos = res;
+      console.log('LLAMADA');
+      console.log(this.productos);
+      this.loading = false;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+
+  editProduct(doc: UpdateProductDto) {
+    this.productsEdit = {...doc};
+    this.eliminarProductos.emit(doc)/* Pendiente codificacion output nombre */
+  }
+  
+
+  // editProduct(product: UpdateProductDto) {
+  //   // Asigna el producto seleccionado a la propiedad selectedProduct
+  //   this.selectedProduct = product;
+
+  //   // Crea un objeto que contiene los valores del formulario
+  //   const formValues = {
+  //     id: product.id,
+  //     title: product.title,
+  //     price: product.price,
+  //     description: product.description,
+  //     images: Array.isArray(product.images) ? product.images.join(', ') : '',
+  //     category: product.category
+  //   };
+
+  //   // Establece los valores en el formulario
+  //   //this.productForm.setValue(formValues);
+  // }
+
+
+  deleteProduct(id: number) {
+    this.productService.deleteProduct(id).subscribe(
+      (res: any) => {
+        console.log(res);
+        this.obtenerProductos();
+      },
+      (err: any) => {
+        console.log(err);
+      }
+    );
+    console.log("delete product");
   }
 
 }
