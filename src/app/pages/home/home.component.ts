91ab1c98da7a01a75b3a9596b8c95d2b009b0dcc
Kevin Rivera <shimpo11.kr@gmail.com>
2023-09-19 13:42:02 -0500

    feat: :art: agregar funcionalidad para cargar productos en el Home
    - Se agregó la funcionalidad para obtener y mostrar los productos en la página de inicio. - Se utiliza el servicio ProductsService para obtener la lista de productos. - Se muestra un indicador de carga mientras se obtienen los datos.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { firstValueFrom } from 'rxjs';
+import { ProductsService } from 'src/app';
+import { ProductModel } from 'src/app/models';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+	selector: 'app-home',
+	templateUrl: './home.component.html',
+	styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+	productos: ProductModel[] = [];
+	loading: boolean = true;
 
-  ngOnInit() {
-  }
+	constructor(private productService: ProductsService) { }
+
+	ngOnInit() {
+		this.obtenerProductos();
+	}
+
+	async obtenerProductos() {
+		try {
+			const res = await firstValueFrom(this.productService.getAllProducts());
+			this.productos = res;
+			console.log('LLAMADA');
+			console.log(this.productos);
+			this.loading = false;
+		} catch (err) {
+			console.log(err);
+		}
+	}
 
 }
